dbad315469ce9eecaea0e8250f751e1000f85af7
madmonn3 <noa.madmon@live.biu.ac.il>
2019-04-21 13:35:51 +0300

    view 1.0 unfinished.

--- FlightSimulator/FlightSimulator/Views/FlightScreen.xaml.cs
+++ FlightSimulator/FlightSimulator/Views/FlightScreen.xaml.cs
@@ -10,23 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulator
+namespace FlightSimulator.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FlightScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FlightScreen : UserControl
     {
-        public MainWindow()
+        public FlightScreen()
         {
             InitializeComponent();
-        }
-
-        private void Joystick_Loaded()
-        {
-
         }
     }
 }
